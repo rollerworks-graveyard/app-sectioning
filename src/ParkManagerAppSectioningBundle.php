28dc1775743bee3edab1ea75476564213f51fd46
Sebastiaan Stok <s.stok@rollerscapes.net>
2016-06-07 16:11:34 +0200

    Fix missing AppSectionsPass in Bundle

--- src/ParkManagerAppSectioningBundle.php
+++ src/ParkManagerAppSectioningBundle.php
@@ -12,10 +12,17 @@
 namespace ParkManager\Bundle\AppSectioning;
 
 use ParkManager\Bundle\AppSectioning\DependencyInjection\AppSectionExtension;
+use ParkManager\Bundle\AppSectioning\DependencyInjection\Compiler\AppSectionsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ParkManagerAppSectioningBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new AppSectionsPass());
+    }
+
     public function getContainerExtension()
     {
         if (null === $this->extension) {
