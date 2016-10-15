ab28f62f5b98d969199274ec1c7bd99efcd3604d
Sebastiaan Stok <s.stok@rollerscapes.net>
2016-10-15 10:40:30 +0200

    Corrrect style

--- src/DependencyInjection/SectioningConfigurator.php
+++ src/DependencyInjection/SectioningConfigurator.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Rollerworks AppSectioningBundle package.
- *
- * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 declare(strict_types=1);
 
 /*
